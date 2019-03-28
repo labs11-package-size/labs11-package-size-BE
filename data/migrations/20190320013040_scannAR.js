6681baf381e97ba845800f49c38d65ccd3ca5405
Jacob Bryan <jtb36688@gmail.com>
2019-03-28 02:41:56 -0400

    Updated database schema, seeded new data with fakerJS
    Added box table to datamodel. Replaced users table oAuth with UID field. Seeded 500 users, 23 products, and 23 shipments with random data. Shipments is missing tracking numbers.

--- data/migrations/20190320013040_scannAR.js
+++ data/migrations/20190320013040_scannAR.js
@@ -9,11 +9,11 @@ exports.up = function(knex, Promise) {
       column.string("password", 32).notNullable();
       column.string("fullName", 128).defaultTo("");
       column.string("email", 128).defaultTo("");
-      column.boolean("oAuth").defaultTo(false);
       column.uuid("uuid");
+      column.string("uid", 32).defaultTo("")
     })
     .createTable("products", column => {
-      column.increments("identifier");
+      column.increments("identifier")
       column.date("lastUpdated", 24)
       column.string("name", 128).notNullable();
       column.string("productDescription", 512).defaultTo("");
@@ -37,25 +37,38 @@ exports.up = function(knex, Promise) {
       column.increments("identifier");
       column.string("label", 24).defaultTo("");
       column.string("url", 512).defaultTo("");
-      column
-        .integer("productId")
-        .unsigned()
-        .references("id")
-        .inTable("products")
-        .onDelete("CASCADE")
-        .onUpdate("CASCADE");
       column.uuid("uuid");
+      column
+      .integer("productId")
+      .unsigned()
+      .references("id")
+      .inTable("products")
+      .onDelete("CASCADE")
+      .onUpdate("CASCADE");
+    })
+    .createTable("boxes", column => {
+      column.decimal("length", 3, 1);
+      column.decimal("width", 3, 1);
+      column.decimal("height", 3, 1);
+      column.uuid("uuid")
+      column
+      .integer("productId")
+      .unsigned()
+      .references("id")
+      .inTable("products")
+      .onDelete("CASCADE")
+      .onUpdate("CASCADE");
     })
     .createTable("shipments", column => {
       column.increments("identifier");
-      column.date("lastUpdated", 24)
-      column.date("dateShipped", 24);
+      column.date("lastUpdated", 24).defaultTo("")
+      column.date("dateShipped", 24).defaultTo("")
       column.string("productName", 128).defaultTo("");
       column.string("shippedTo", 512).defaultTo("");
       column.string("trackingNumber", 128).defaultTo("");
       column.string("carrierName", 128).defaultTo("");
       column.string("shippingType", 128).defaultTo("");
-      column.integer("status");
+      column.integer("status").defaultTo("");
       column.uuid("uuid");
       column
         .integer("productId")
@@ -72,5 +85,6 @@ exports.down = function(knex, Promise) {
     .dropTableIfExists("users")
     .dropTableIfExists("products")
     .dropTableIfExists("product_assets")
+    .dropTableIfExists("boxes")
     .dropTableIfExists("shipments");
 };
---
+++ data/seeds/users3.js
@@ -0,0 +1,25 @@
+const faker = require("faker");
+const bcrypt = require("bcryptjs");
+
+const fakerUsers = () => {
+  let usersArray = []
+  for (i = 201; i <= 300; i++) {
+    usersArray.push({
+      identifier: i,
+      username: faker.internet.userName(),
+      password: bcrypt.hashSync(faker.internet.password(), 10),
+      fullName: faker.name.findName(),
+      email: faker.internet.email(),
+      uuid: faker.random.uuid()
+    });
+  }
+  return usersArray;
+};
+
+exports.seed = function(knex, Promise) {
+  // Deletes ALL existing entries
+  return knex("users")
+    .then(function() {
+      return knex("users").insert(fakerUsers());
+    });
+};
