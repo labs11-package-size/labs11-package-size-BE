2d1c4382bf2904bdaf3bbc59baf95284867386d1
Jacob Bryan <jtb36688@gmail.com>
2019-04-03 01:47:03 -0400

    refreshed seeding

--- data/seeds/shipments2.js
+++ data/seeds/shipments2.js
@@ -25,7 +25,7 @@ const fakerShipments = () => {
 
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex('shipments').truncate()
+  return knex('shipments')
     .then(function () {
       // Inserts seed entries
       return knex('shipments').insert(fakerShipments());
